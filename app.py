39d488b6be45e84396aad7713245497742bdb2e5
vchilikov <vchilikov@gmail.com>
2017-01-29 00:48:45 +0300

    Добавил файл настроек и возможность указания порта при запуске.

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import asyncio
 import concurrent.futures
+import sys
 
+import aiohttp_jinja2
 import asyncio_redis
+import jinja2
 from aiohttp import web
 from aiohttp.web_exceptions import HTTPBadRequest
 
 from messages import Messages
-import aiohttp_jinja2
-import jinja2
+from settings import REDIS_CONNECTION, MESSAGES_CHANNEL, DEFAULT_PORT
 
 messages = Messages.get_instance()
 
 
-async def example(app):
-    connection = await asyncio_redis.Connection.create('localhost', 6379)
+async def message_subscribe():
+    connection = await asyncio_redis.Connection.create(**REDIS_CONNECTION)
     try:
         subscriber = await connection.start_subscribe()
-        await subscriber.subscribe(['my-channel'])
+        await subscriber.subscribe([MESSAGES_CHANNEL])
         while True:
             reply = await subscriber.next_published()
             messages.append(repr(reply.value))
@@ -34,9 +36,9 @@ async def index(request):
 
 async def post_index(request):
     data = await request.post()
-    connection = await asyncio_redis.Connection.create('localhost', 6379)
+    connection = await asyncio_redis.Connection.create(**REDIS_CONNECTION)
     try:
-        await connection.publish('my-channel', data['message'])
+        await connection.publish(MESSAGES_CHANNEL, data['message'])
     finally:
         connection.close()
     return web.json_response({
@@ -56,13 +58,16 @@ async def long_poll(request):
     return web.json_response({'status': 'ok', 'result': messages})
 
 
-app = web.Application()
-aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('./templates'))
+if __name__ == '__main__':
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else DEFAULT_PORT
 
-app.router.add_get('/', index)
-app.router.add_post('/', post_index)
-app.router.add_get('/long_poll/', long_poll)
+    app = web.Application()
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('./templates'))
 
-app['redis_listener'] = app.loop.create_task(example(app))
+    app.router.add_get('/', index)
+    app.router.add_post('/', post_index)
+    app.router.add_get('/long_poll/', long_poll)
 
-web.run_app(app, port=8080)
+    app['redis_listener'] = app.loop.create_task(message_subscribe())
+
+    web.run_app(app, port=port)
---
+++ settings.py
@@ -0,0 +1,8 @@
+REDIS_CONNECTION = {
+    'host': 'localhost',
+    'port': 6379
+}
+
+MESSAGES_CHANNEL = 'messages_channel'
+
+DEFAULT_PORT = 8080
